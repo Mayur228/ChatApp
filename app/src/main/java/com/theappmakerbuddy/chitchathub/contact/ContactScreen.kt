3238ee5d985ab61932c09971561d2ad7a82a1ce5
mayur <mayursinh.parmar@knoxpo.com>
2024-03-08 15:07:56 +0530

    remove unused code

--- app/src/main/java/com/theappmakerbuddy/chitchathub/contact/ContactScreen.kt
+++ app/src/main/java/com/theappmakerbuddy/chitchathub/contact/ContactScreen.kt
@@ -3,14 +3,11 @@ package com.theappmakerbuddy.chitchathub.contact
 import android.util.Log
 import android.widget.Toast
 import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material.IconButton
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -23,7 +20,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -77,9 +73,6 @@ fun Contact(viewModel: ContactViewModel = hiltViewModel()) {
     Scaffold(
         topBar = {
             StandardToolbar(
-                onBackArrowClicked = {
-//                    onEvent(HomeUiEvents.NavigateBack)
-                },
                 title = {
                     Box(
                         modifier = Modifier
@@ -87,7 +80,7 @@ fun Contact(viewModel: ContactViewModel = hiltViewModel()) {
                         contentAlignment = Alignment.Center
                     ) {
                         Text(
-                            text = "Contact",
+                            text = "Contacts",
                             fontSize = 25.sp,
                             fontWeight = FontWeight.Bold,
                             color = Color.White,
@@ -99,17 +92,6 @@ fun Contact(viewModel: ContactViewModel = hiltViewModel()) {
                 },
                 modifier = Modifier.fillMaxWidth(),
                 showBackArrow = false,
-                navActions = {
-                    IconButton(onClick = {
-//                        onEvent(HomeUiEvents.OnSearchClick)
-                    }) {
-                        Image(
-                            modifier = Modifier.size(27.dp),
-                            painter = painterResource(id = R.drawable.ic_add_contact),
-                            contentDescription = null,
-                        )
-                    }
-                }
             )
         }
     ) { innerPadding ->
@@ -145,7 +127,7 @@ fun Contact(viewModel: ContactViewModel = hiltViewModel()) {
         }
 
         UserList(
-            modifier = Modifier,
+            modifier = Modifier.padding(innerPadding),
             contactList = filteredContactList.toList()
         )
     }
@@ -155,7 +137,6 @@ fun Contact(viewModel: ContactViewModel = hiltViewModel()) {
 fun UserList(modifier: Modifier, contactList: List<User>?) {
     LazyColumn(
         modifier = modifier
-            .padding(2.dp),
     ) {
         contactList?.forEach {
             item {
